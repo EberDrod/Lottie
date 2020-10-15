3b440fa23ad7cde652a6e9646536ce4b32f7c695
EbPeña <eberg.pena@gmail.com>
2020-10-15 01:23:10 -0500

    hack de After vs JSON Archivo para animaciones 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import ControlledLottie from "./component/ControlledLottie";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ControlledLottie />
+      </div>
+    );
+  }
+}
+
+export default App;
